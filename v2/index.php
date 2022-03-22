4dd85af75d575b1c55e5ae4f7ca4bfc2603d0702
Will Clarke <clarke19w@gmail.com>
2022-03-22 19:41:32 +0000

    feat: finish v2 until new endpoints

--- v2/index.php
+++ v2/index.php
@@ -1,7 +1,6 @@
 <?php 
 header("Content-Type: application/json");
-function response($status, $data, $status_message){
-    if(empty(trim($status_message))){
+function response($status, $data){
         switch ($status){
             case 200:
                 $status_message = "ok";
@@ -18,10 +17,53 @@ function response($status, $data, $status_message){
             case 500:
                 $status_message = "server error";
         }
-    }
     $response['status'] = $status;
     $response['status_message'] = $status_message;
     $response['data'] = $data;
     $json = json_encode($response);
     echo $json;
+}
+function rand_array($array){
+    $count = count($array)-1;
+    return $array[rand(0,$count)];
+}
+/*
+ENDPOINTS
+https://api.wclarke.dev/v2/?key=******&end=image&content=random-duck
+*/
+if ($_SERVER['REQUEST_METHOD']=='GET'){
+    $api_key = $_GET['key'];
+    $endpoint = $_GET['end'];
+    $content = $_GET['content'];
+    if (empty(trim($api_key))) response(400, "Missing api credentials");
+    if (empty(trim($endpoint))) response(400, "Missing endpoint");
+    if (empty(trim($content))) response(400, "Missing content type");
+    if ($api_key != 'will8') response(400, "Invalid api credentials");
+    switch($endpoint){
+        case "image":
+            switch($content){
+                case "random-duck":
+                    $duck_urls = [
+                        'https://api.wclarke.dev/media/duck1.jpg',
+                        'https://api.wclarke.dev/media/duck2.jpg',
+                        'https://api.wclarke.dev/media/duck3.jpg',
+                        'https://api.wclarke.dev/media/duck4.jpg',
+                        'https://api.wclarke.dev/media/duck5.jpg',
+                        'https://api.wclarke.dev/media/duck6.jpg',
+                        'https://api.wclarke.dev/media/duck7.jpg',
+                        'https://api.wclarke.dev/media/duck8.jpg',
+                        'https://api.wclarke.dev/media/duck9.jpg',
+                        'https://api.wclarke.dev/media/duck10.jpg',
+                        'https://api.wclarke.dev/media/robs_duck.jpg'
+                    ];
+                    $random_duck = rand_array($duck_urls);
+                    response(200, $random_duck);
+                break;
+                default:
+                    response(400, "Invalid content type");
+            }
+            break;
+        default:
+            response(400, "Invalid endpoint");
+    }
 }
