84628f973050a7c7d446b64315a8af5f981e429e
Will Clarke <clarke19w@gmail.com>
2022-03-22 20:00:05 +0000

    feat: made it work

--- v2/index.php
+++ v2/index.php
@@ -1,24 +1,8 @@
 <?php 
 header("Content-Type: application/json");
 function response($status, $data){
-        switch ($status){
-            case 200:
-                $status_message = "ok";
-                break;
-            case 400:
-                $status_message = "bad request";
-                break;
-            case 404:
-                $status_message = "not found";
-            break;
-            case 403:
-                $status_message = "forbidden";
-                break;
-            case 500:
-                $status_message = "server error";
-        }
+    header("HTTP/1.1 ".$status);
     $response['status'] = $status;
-    $response['status_message'] = $status_message;
     $response['data'] = $data;
     $json = json_encode($response);
     echo $json;
@@ -35,35 +19,38 @@ if ($_SERVER['REQUEST_METHOD']=='GET'){
     $api_key = $_GET['key'];
     $endpoint = $_GET['end'];
     $content = $_GET['content'];
-    if (empty(trim($api_key))) response(400, "Missing api credentials");
-    if (empty(trim($endpoint))) response(400, "Missing endpoint");
-    if (empty(trim($content))) response(400, "Missing content type");
-    if ($api_key != 'will8') response(400, "Invalid api credentials");
-    switch($endpoint){
-        case "image":
-            switch($content){
-                case "random-duck":
-                    $duck_urls = [
-                        'https://api.wclarke.dev/media/duck1.jpg',
-                        'https://api.wclarke.dev/media/duck2.jpg',
-                        'https://api.wclarke.dev/media/duck3.jpg',
-                        'https://api.wclarke.dev/media/duck4.jpg',
-                        'https://api.wclarke.dev/media/duck5.jpg',
-                        'https://api.wclarke.dev/media/duck6.jpg',
-                        'https://api.wclarke.dev/media/duck7.jpg',
-                        'https://api.wclarke.dev/media/duck8.jpg',
-                        'https://api.wclarke.dev/media/duck9.jpg',
-                        'https://api.wclarke.dev/media/duck10.jpg',
-                        'https://api.wclarke.dev/media/robs_duck.jpg'
-                    ];
-                    $random_duck = rand_array($duck_urls);
-                    response(200, $random_duck);
-                break;
+    switch ($api_key){
+        case "wclarkey":
+            switch($endpoint){
+                case "image":
+                    switch($content){
+                        case "random-duck":
+                            $duck_urls = [
+                                'https://api.wclarke.dev/media/duck1.jpg',
+                                'https://api.wclarke.dev/media/duck2.jpg',
+                                'https://api.wclarke.dev/media/duck3.jpg',
+                                'https://api.wclarke.dev/media/duck4.jpg',
+                                'https://api.wclarke.dev/media/duck5.jpg',
+                                'https://api.wclarke.dev/media/duck6.jpg',
+                                'https://api.wclarke.dev/media/duck7.jpg',
+                                'https://api.wclarke.dev/media/duck8.jpg',
+                                'https://api.wclarke.dev/media/duck9.jpg',
+                                'https://api.wclarke.dev/media/duck10.jpg',
+                                'https://api.wclarke.dev/media/robs_duck.jpg'
+                            ];
+                            $random_duck = rand_array($duck_urls);
+                            response(200, $random_duck);
+                        break;
+                        default:
+                            response(400, "Invalid content type");
+                    }
+                    break;
                 default:
-                    response(400, "Invalid content type");
+                    response(400, "Invalid endpoint");
             }
-            break;
+        break;
         default:
-            response(400, "Invalid endpoint");
+            response(400, "Invalid api credentials");
     }
+   
 }
